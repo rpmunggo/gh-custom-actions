54d1d00c437d8eb4a1dee5a7c4246d919c090acf
Rogelio Pascual III <rogeliopascualiii@gmail.com>
2023-01-17 10:49:02 +0800

    Added AWS s3 deployment action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run () {
+    // 1) Get someinput values
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+
+     // 2) Upload Files
+     const sr3Uri= `s3://${bucket}`
+     exec.exec(`aws s3 sync ${distFolder} ${sr3Uri} --region ${bucketRegion}`)
+
     core.notice('Hello from my custom JavaScript Action!')
 }
 
